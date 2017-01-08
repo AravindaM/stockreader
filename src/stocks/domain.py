cb4e32832ae857d9b944b55e2c84aa75119e8e85
Julian Espinel <julianespinel@gmail.com>
2017-01-07 20:37:13 -0500

    Save stocks data in influxdb.

--- src/stocks/domain.py
+++ src/stocks/domain.py
@@ -1,12 +1,12 @@
 from infrastructure import log
-from datetime import date, timedelta
 
 logger = log.get_logger("domain")
 
 class Domain:
 
-    def __init__(self, mongo, download):
+    def __init__(self, mongo, influx, download):
         self.mongo = mongo
+        self.influx = influx
         self.download = download
 
     def download_and_save_stock_current_data(self, stock):
@@ -14,12 +14,14 @@ class Domain:
         logger.info("stock %s", quote)
         stock_current_data = self.download.get_stock_current_data(quote)
         self.mongo.upsert_stock_current_data(quote, stock_current_data)
+        self.influx.update_stock_current_data(quote, stock_current_data)
 
     def download_and_save_stock_historical_data(self, initialDate, finalDate, stock):
         quote = stock["symbol"]
         logger.info('stocks %s, %s, %s', initialDate, finalDate, quote)
         stock_historical_data_array = self.download.get_stock_historical_data(initialDate, finalDate, quote)
         self.mongo.save_stock_historical_data(quote, stock_historical_data_array)
+        self.influx.save_stock_historical_data(quote, stock_historical_data_array)
 
     def stock_exists(self, quote):
         return self.mongo.stock_exists(quote)
