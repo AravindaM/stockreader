7bcaacaf397052225d23c51a35be6630ebea59f2
Julian Espinel <julianespinel@gmail.com>
2017-01-07 20:38:11 -0500

    Remove not needed constant.

--- src/infrastructure/time_series.py
+++ src/infrastructure/time_series.py
@@ -6,13 +6,10 @@ logger = log.get_logger("time_series")
 
 class TimeSeries:
 
-    DB_NAME = "stockreaderdb"
     NUMBER_OF_WORKERS = 5
 
     def __init__(self, influx_client):
         self.influx = influx_client
-        self.influx.create_database(self.DB_NAME)
-        self.influx.switch_database(self.DB_NAME)
         self.thread_pool = ThreadPoolExecutor(max_workers=self.NUMBER_OF_WORKERS)
 
     def save_async(self, measurement, tags_dict, fields_dict):
