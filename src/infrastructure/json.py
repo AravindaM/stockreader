68676fc3e31578f25345abd439664c82442c2f5a
Julian Espinel <julianespinel@gmail.com>
2017-01-09 22:47:56 -0500

    Add remove_single_quotes_from_json function.

--- src/infrastructure/json.py
+++ src/infrastructure/json.py
@@ -7,3 +7,11 @@ def json_keys_to_lower_and_snake_case(json):
         return dict((inflection.underscore(key), json_keys_to_lower_and_snake_case(value)) for key, value in json.items())
     else:
         return json
+
+def remove_single_quotes_from_json(json):
+    if isinstance(json, list):
+        return [remove_single_quotes_from_json(element) for element in json]
+    elif isinstance(json, dict):
+        return dict((key, remove_single_quotes_from_json(value)) for key, value in json.items())
+    else:
+        return json.replace("'", "")
